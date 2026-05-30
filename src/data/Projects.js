c56507ada0eb37736549555926d53b476374e640
Chandoeurn Chorm <chandoeurn708@gmail.com>
2026-05-30 11:36:12 +0700

    Update project

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,5 +1,6 @@
 import Pj01 from "../assets/image/Project01.png";
 import Pj02 from "../assets/image/Project02.png";
+import Pj03 from "../assets/image/Project03.png";
 
 export const project = [
   {
@@ -9,17 +10,27 @@ export const project = [
     description:
       "A modern, responsive web application designed with a focus on seamless user experience, intuitive navigation, and clean aesthetics.",
     tags: ["HTML", "CSS"],
-    liveLink: "https://zendoone-me.vercel.app/", // Replace with your link
-    githubLink: "https://github.com/Doeurn-developer/Zendoone", // Replace with your link
+    liveLink: "https://zendoone-me.vercel.app/",
+    githubLink: "https://github.com/Doeurn-developer/Zendoone",
   },
   {
-    id: 2, // Fixed duplicate ID
+    id: 2,
     img: Pj02,
     name: "D-Shop",
     description:
       "A feature-rich e-commerce platform boasting full product catalogs, dynamic filtering, interactive shopping carts, and secure checkout simulation.",
     tags: ["Vue 3", "Tailwind CSS"],
-    liveLink: "https://finally-project-bay.vercel.app/", // Replace with your link
-    githubLink: "https://github.com/Doeurn-developer/Finally-project", // Replace with your link
+    liveLink: "https://finally-project-bay.vercel.app/",
+    githubLink: "https://github.com/Doeurn-developer/Finally-project",
+  },
+  {
+    id: 3,
+    img: Pj03,
+    name: "Food Order",
+    description:
+      "A feature-rich e-commerce platform boasting full product catalogs, dynamic filtering, interactive shopping carts, and secure checkout simulation.",
+    tags: ["Vue 3", "Tailwind CSS"],
+    liveLink: "https://order-food-kappa-seven.vercel.app/",
+    githubLink: "https://github.com/Doeurn-developer/Order-Food-Etec",
   },
 ];
